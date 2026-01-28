db46694d6fc8b457c7d80b6abe5ee7060fa4366d
burked9 <burked9@gmail.com>
2026-01-28 17:47:31 +0000

    Fix SITEURL to use correct GitHub username (burked9)

--- publishconf.py
+++ publishconf.py
@@ -7,7 +7,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'https://danielburke.github.io/rally50'
+SITEURL = 'https://burked9.github.io/rally50'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
